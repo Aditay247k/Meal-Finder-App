39743251246a866123d93d37776040626becb61c
Aditay247k <aditayroy354@gmail.com>
2022-03-14 02:38:49 +0530

    Updates

---
+++ precache-manifest.11b18b0315adc7c652b668665f303dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60a13f58bf23ca50a1d8",
+    "url": "/Meal-Finder-App/static/css/main.8147953f.chunk.css"
+  },
+  {
+    "revision": "60a13f58bf23ca50a1d8",
+    "url": "/Meal-Finder-App/static/js/main.60a13f58.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/Meal-Finder-App/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "efa56e5a81964974b35e",
+    "url": "/Meal-Finder-App/static/js/2.efa56e5a.chunk.js"
+  },
+  {
+    "revision": "56ce0042413636bb8c50",
+    "url": "/Meal-Finder-App/static/js/runtime~main.56ce0042.js"
+  },
+  {
+    "revision": "b976092e5d1a7e5fea8c84f5bde683b7",
+    "url": "/Meal-Finder-App/index.html"
+  }
+];
